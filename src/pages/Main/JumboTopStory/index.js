27168be5589c4d2348d69f7edde76ca1306dcb4c
costadaniel <dancosta0601@gmail.com>
2019-07-17 12:41:22 -0300

    Adição do Prettier

--- src/pages/Main/JumboTopStory/index.js
+++ src/pages/Main/JumboTopStory/index.js
@@ -5,16 +5,14 @@ import PropTypes from 'prop-types';
 import Reactotron from 'reactotron-react-native';
 
 import LinkPreview from 'react-native-link-preview';
-import {
-  Container, LoadingIndicator, InfoContainer, Title,
-} from './styles';
+import { Container, LoadingIndicator, InfoContainer, Title } from './styles';
 
 import api from '~/services/api';
 
 export default class Main extends Component {
   static propTypes = {
     id: PropTypes.string.isRequired,
-  }
+  };
 
   state = {
     mediaUrl: '',
@@ -52,26 +50,29 @@ export default class Main extends Component {
 
     return (
       <Container
-        source={mediaUrl.length > 0 ? { uri: mediaUrl } : require('../../../../assets/images/white-bg.jpg')}
+        source={
+          mediaUrl.length > 0
+            ? { uri: mediaUrl }
+            : require('../../../../assets/images/white-bg.jpg')
+        }
         style={{
           shadowColor: '#000',
           shadowOffset: {
             width: 0,
             height: 4,
           },
-          shadowOpacity: 0.30,
+          shadowOpacity: 0.3,
           shadowRadius: 4.65,
           elevation: 8,
         }}
       >
-        {onLoading
-          ? <LoadingIndicator />
-          : (
-            <InfoContainer>
-              <Title>{storyInfos.title}</Title>
-            </InfoContainer>
-          )
-        }
+        {onLoading ? (
+          <LoadingIndicator />
+        ) : (
+          <InfoContainer>
+            <Title>{storyInfos.title}</Title>
+          </InfoContainer>
+        )}
       </Container>
     );
   }
