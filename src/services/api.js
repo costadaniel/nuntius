04e2ad06e5d11fb10b5edd0be78d0aca8af059c8
costadaniel <dancosta0601@gmail.com>
2024-01-10 22:51:01 -0300

    Remove the old app project and move then to Expo

--- src/services/api.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: 'https://hacker-news.firebaseio.com/v0',
-});
-
-export default api;
--- src/styles/index.js
+++
@@ -1,5 +0,0 @@
-import colors from './colors';
-import metrics from './metrics';
-import global from './global';
-
-export { colors, metrics, global };
